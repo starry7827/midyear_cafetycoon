92f865cfb466860f5e388d8d1c633885c4baff7f
Miguel Sylvestre <miguelsylvestre@Miguels-MacBook-Air.local>
2026-01-29 13:27:39 -0500

    added wall and floor textures, added some hitboxes, bottom border, added character sprite

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 100f;
+    Rigidbody2D rb;
+    Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput = moveInput.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * (speed);
+    }
+}
