331ac39bdda3f74d1088c277d8b0b7ef218ad072
Miguel Sylvestre <miguelsylvestre@Mac.lan>
2026-01-30 01:43:24 -0500

    fixed scaling, fixed hitboxes, added player animations, added details to mm ui, added 'Café Haven,'

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 20f;
     Rigidbody2D rb;
     Vector2 moveInput;
+    public Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -16,10 +18,17 @@ public class PlayerMovement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput = moveInput.normalized;
+        if (moveInput != Vector2.zero) {
+            animator.SetBool("Moving", true);
+            animator.SetFloat("MoveX", moveInput.x);
+        }
+        else {
+            animator.SetBool("Moving", false);
+        }
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * (speed);
+        rb.linearVelocity = moveInput * speed;
     }
 }
