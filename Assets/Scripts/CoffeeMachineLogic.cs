cbf7a1ff7fa079f331ada5c2c62ebf624bfa10a7
starry7827 <pydalal7@gmail.com>
2026-02-03 16:50:02 -0500

    fixed coffee machine button null error and added new grate + shadows + nozzel for actual coffee making part

--- Assets/Scripts/CoffeeMachineLogic.cs
+++ Assets/Scripts/CoffeeMachineLogic.cs
@@ -15,59 +15,72 @@ public class CoffeeMachineLogic : MonoBehaviour
     // Coffee Machine Logic 
     // ------------------------------------------------------------------------------------------------------
 
-    void Start() {
+    void Start()
+    {
         currentCup = CupType.NoCup;
         drink = Drink.NoDrink;
         buttonBg.color = new Color32(247, 103, 103, 255);
         button.interactable = false;
     }
 
-    public void reset() {
+    public void reset()
+    {
         currentCup = CupType.NoCup;
         drink = Drink.NoDrink;
         buttonBg.color = new Color32(247, 103, 103, 255);
         button.interactable = false;
     }
-    
-    public void HotLatte() {
+
+    public void HotLatte()
+    {
         drink = Drink.HotLatte;
         DrinkSelected = true;
         MachineState = "DrinkSelected";
         tryEnable();
     }
-    public void ColdLatte() {
+    public void ColdLatte()
+    {
         drink = Drink.ColdLatte;
         MachineState = "DrinkSelected";
         tryEnable();
     }
-    public void Espresso() {
+    public void Espresso()
+    {
         drink = Drink.Espresso;
         MachineState = "DrinkSelected";
         tryEnable();
     }
 
-    public void Pour() {
+    public void Pour()
+    {
         MachineState = "Pouring";
         Debug.Log("pouring: " + coffee);
     }
 
-    public void setCup(CupType cupType) {
+    public void setCup(CupType cupType)
+    {
         currentCup = cupType;
         Debug.Log("Set Cup Type to: " + cupType);
     }
 
     // new Color32(255, 100, 50, 255);
 
-    public void tryEnable() {
-        if (drink == Drink.HotLatte && currentCup == CupType.HotCup) {
-            button.interactable = true;
-            Debug.Log("Can Pour " + drink + " in " + currentCup);
-            buttonBg.color = new Color32(124, 247, 103, 255);
-        }
-        if (drink == Drink.ColdLatte && currentCup == CupType.ColdCup) {
-            button.interactable = true;
-            Debug.Log("Can Pour " + drink + " in " + currentCup);
-            buttonBg.color = new Color32(124, 247, 103, 255);
+    public void tryEnable()
+    {
+        if (drink != null && currentCup != null)
+        {
+            if (drink == Drink.HotLatte && currentCup == CupType.HotCup)
+            {
+                button.interactable = true;
+                Debug.Log("Can Pour " + drink + " in " + currentCup);
+                buttonBg.color = new Color32(124, 247, 103, 255);
+            }
+            if (drink == Drink.ColdLatte && currentCup == CupType.ColdCup)
+            {
+                button.interactable = true;
+                Debug.Log("Can Pour " + drink + " in " + currentCup);
+                buttonBg.color = new Color32(124, 247, 103, 255);
+            }
         }
     }
 }
