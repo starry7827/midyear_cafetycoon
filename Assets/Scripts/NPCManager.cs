0df44667fa693c27726294f150a4994ca8cdaf9c
starry7827 <pydalal7@gmail.com>
2026-02-08 17:08:16 -0500

    added array of npcs to npc manager, adjusted spawn pos for npcs and added max npcs to the line

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 
 public class NPCManager : MonoBehaviour
 {
-    public GameObject npc1Prefab;
-    public float spawnInterval = 30f; // this can be changed eventually to something that increases as time goes on or something random or we can make it change after one spawns i just made it this for testing
-    public Vector2 spawnPos = new Vector2(103f, -54f);
+    public GameObject[] npcPrefabs;
+    public float spawnInterval = 10f; // this can be changed eventually to something that increases as time goes on or something random or we can make it change after one spawns i just made it this for testing
+    public Vector2 spawnPos = new Vector2(103f, -38f);
     public Vector2 frontPos = new Vector2(-1f, 6f);
-    public float spacing = 10f; // this can also be changed its how far inbetween the npcs
+    public float spacing = 25f; // this can also be changed its how far inbetween the npcs
     private float timer = 0f;
+    public int maxNPCS = 5;
+    private List<GameObject> available;
     private List<NPCMovement> line = new List<NPCMovement>();
+
 
     void Start()
     {
-
+        available = new List<GameObject>(npcPrefabs);
     }
 
     // Update is called once per frame
@@ -31,7 +34,19 @@ public class NPCManager : MonoBehaviour
 
     void spawnNPC()
     {
-        GameObject npc = Instantiate(npc1Prefab, spawnPos, Quaternion.identity);
+        if (line.Count >= maxNPCS)
+            return;
+
+
+        if (available.Count == 0)
+            return;
+
+        int npcI = Random.Range(0, available.Count);
+        GameObject chosenPrefab = available[npcI];
+        available.RemoveAt(npcI);
+
+        GameObject npc = Instantiate(chosenPrefab, spawnPos, Quaternion.identity);
+
         NPCMovement npcMove = npc.GetComponent<NPCMovement>();
 
         line.Add(npcMove);
